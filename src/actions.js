aa632634736446048309a9e895707b1c16098533
Kamil Mówiński <kamil.mowinski@stxnext.pl>
2018-05-25 22:17:24 +0200

    Move action to component

--- src/actions.js
+++ src/actions.js
@@ -9,24 +9,6 @@ export const categoryAdded = () => ({
     type: 'CATEGORY_ADDED',
 });
 
-export const hideCategoryAdded = () => ({
-    type: 'HIDE_CATEGORY_ADDED',
-});
-
-export const showCategoryAdded = () => ({
-    type: 'SHOW_CATEGORY_ADDED',
-});
-
-export const setCreatingState = (isCreating) => ({
-    type: 'SET_CREATING_STATE',
-    isCreating
-});
-
-export const setCategoryAdded = () => (dispatch) => {
-    dispatch(showCategoryAdded());
-    setTimeout(() => dispatch(hideCategoryAdded()), 5000);
-};
-
 export const fetchCategories = () => (dispatch) => {
     const categories = firebase.database().ref('/categories');
     categories.on(
@@ -35,14 +17,9 @@ export const fetchCategories = () => (dispatch) => {
     );
 };
 
-export const addCategory = (categoryName) => (dispatch) => {
-    dispatch(setCreatingState(true));
+export const addCategory = (categoryName) => async (dispatch) => {
     firebase.database()
         .ref('/categories')
         .push()
-        .set(categoryName)
-        .then(() => {
-            dispatch(setCreatingState(false));
-            dispatch(setCategoryAdded());
-        });
+        .set(categoryName);
 };
--- src/reducers/category.js
+++ src/reducers/category.js
@@ -1,16 +1,7 @@
-export const category = (state = {categories: {}, isCreating: false, isCreatedNewCategory: false}, action) => {
+export const category = (state = {categories: {}}, action) => {
     switch (action.type) {
         case 'FETCH_CATEGORY_SUCCESS':
             return Object.assign({}, state, {categories: action.categories});
-
-        case 'SET_CREATING_STATE':
-            return Object.assign({}, state, {isCreating: action.isCreating});
-
-        case 'SHOW_CATEGORY_ADDED':
-            return Object.assign({}, state, {isCreatedNewCategory: true});
-
-        case 'HIDE_CATEGORY_ADDED':
-            return Object.assign({}, state, {isCreatedNewCategory: false});
 
         default:
             return state;
